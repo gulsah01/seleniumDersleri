0ed70444c4ae7ed42363ef0ccd916c9daf9dc447
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-09-24 12:41:23 +0100

    Please enter the commit message for your changes. Lines starting x wqewq

---
+++ src/test/java/tests/practice/P03.java
@@ -0,0 +1,39 @@
+package tests.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class P03 {
+    public static void main(String[] args) {
+/*
+1) Browser açın
+2) “http://practice.automationtesting.in/” adresine gidin
+3) Sİtenin başlığını alıp yazdırın
+4) Sitenin url adresini alıp yazdırın
+5) Başlığın Practice ve Url'in  ise practice içerip içermediğini test edin
+6) Pencereyi kapatın
+ */
+        System.setProperty("webdriver.chrome.driver","kurulumDosyalari/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://practice.automationtesting.in/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        if (driver.getTitle().contains("Practice")){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TESR FAILED");
+        }
+        if (driver.getCurrentUrl().contains("practice")){
+            System.out.println("url ractice kelimesi iceriyor\nTest passed");
+        }else {
+            System.out.println("Url test failed");
+        }
+        driver.close();
+
+
+    }
+}
---
+++ src/test/java/tests/practice/P04.java
@@ -0,0 +1,27 @@
+package tests.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class P04 {
+    public static void main(String[] args) {
+        /*
+        ..Exercise3...
+Navigate to  https://testpages.herokuapp.com/styled/index.html
+Click on  Calculater under Micro Apps
+Type any number in the first input
+Type any number in the second input
+Click on Calculate
+Get the result
+Print the result
+  */
+        System.setProperty("webdriver.chrome.driver","kurulumDosyalari/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://testpages.herokuapp.com/styled/index.html");
+
+    }
+}
