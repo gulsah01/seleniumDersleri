b51f1736bdec3b50da6dc79dd5117ae36fc1f67a
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-09-26 12:33:24 +0100

    update

--- src/test/java/tests/day04_xPath_cssSelector/C04_XPath_TextKullanimi.java
+++ src/test/java/tests/day04_xPath_cssSelector/C04_XPath_TextKullanimi.java
@@ -7,43 +7,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C02_XPath {
+public class C04_XPath_TextKullanimi {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","kurulumDosyalari/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-        //1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        //https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
         Thread.sleep(2000);
         //2- Add Element butonuna basin
-        driver.findElement(By.xpath("//button[@*='addElement()']")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[text() = 'Add Element']")).click();
         //3- Delete butonu’nun gorunur oldugunu test edin
-        WebElement deleteButonu = driver.findElement(By.xpath("//button[@*='deleteElement()']"));
+        Thread.sleep(2000);
+        WebElement deleteButonu = driver.findElement(By.xpath("//*[text() = 'Delete']"));
         if (deleteButonu.isDisplayed()){
-            System.out.println("Delete butonu testi PASSED");
+            System.out.println("Delete buton testi PASSED");
         }else{
-            System.out.println("Delete butonu testi FAILED");
+            System.out.println("Delete buton testi FAILED");
         }
         //4- Delete tusuna basin
         deleteButonu.click();
         Thread.sleep(2000);
         //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
-        // WebElement addRemoveYaziElementi = driver.findElement(By.tagName("h3"));
-        WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//h3"));
 
+        WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//*[text() = 'Add/Remove Elements']"));
         if (addRemoveYaziElementi.isDisplayed()){
-            System.out.println("Add remove yazi testi PASSED");
-        }else{
-            System.out.println("Add remove yazi testi FAILED");
+            System.out.println("Add/Remove yazi testi PASSED");
+        }else {
+            System.out.println("Add/Remove yazi testi FAILED");
         }
-
         Thread.sleep(2000);
         driver.close();
     }
-
-
-    }
-
+}
